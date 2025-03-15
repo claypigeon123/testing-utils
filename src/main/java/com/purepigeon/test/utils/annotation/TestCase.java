1758ede8954352a1501cd6108590240bf0d89617
claypigeon123 <n.zoltan18@gmail.com>
2025-03-15 20:05:30 +0100

    javadoc for TestCase annotation

--- src/main/java/com/purepigeon/test/utils/annotation/TestCase.java
+++ src/main/java/com/purepigeon/test/utils/annotation/TestCase.java
@@ -1,7 +1,43 @@
 package com.purepigeon.test.utils.annotation;
 
+import com.purepigeon.test.utils.extension.TestingUtilsExtension;
+
 import java.lang.annotation.*;
 
+/**
+ * <p>
+ *     Annotation to customize the {@code testCase} argument for a given test method, resolved by
+ *     {@link TestingUtilsExtension}.
+ * </p>
+ * <p>
+ *     Omission of this annotation means that the {@code testCase} argument will be the test method name.
+ * </p>
+ * <p>
+ *     For example, given the following test method:
+ *     <pre>
+ *     {@code
+ *         @Test
+ *         void testMethod(String testCase) {
+ *             // ...
+ *         }
+ *     }
+ *     </pre>
+ *     The {@code testCase} value would be {@code testMethod}.
+ * </p>
+ * <p>
+ *     And given the following test method:
+ *     <pre>
+ *     {@code
+ *         @Test
+ *         @TestCase("someTestMethod_200")
+ *         void testMethod(String testCase) {
+ *             // ...
+ *         }
+ *     }
+ *     </pre>
+ *     The {@code testCase} value would be {@code someTestMethod_200}.
+ * </p>
+ */
 @Inherited
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
