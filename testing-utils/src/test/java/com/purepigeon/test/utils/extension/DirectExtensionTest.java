ae743cf0ca12eb8858770a07d18ab40781ceae62
claypigeon123 <n.zoltan18@gmail.com>
2025-05-21 19:29:58 +0200

    remove raw test from lib module

--- testing-utils/src/test/java/com/purepigeon/test/utils/extension/DirectExtensionTest.java
+++ testing-utils/src/test/java/com/purepigeon/test/utils/extension/DirectExtensionTest.java
@@ -1,6 +1,5 @@
-package com.purepigeon.test.utils.raw;
+package com.purepigeon.test.utils.extension;
 
-import com.purepigeon.test.utils.extension.TestingUtilsExtension;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
